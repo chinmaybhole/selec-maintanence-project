b4f706acaff0d8517fd7e33c54dfc9a96e129994
Abbas Ahmedabadwala <abbinira2712@gmail.com>
2023-02-23 21:13:03 +0530

    mod in location api

--- Controllers/admin.controllers.js
+++ Controllers/admin.controllers.js
@@ -751,7 +751,7 @@ const addLocation = async (req, res) => {
                          */
                         if (typeof assets !== 'undefined') {
                             let mapdata = await Promise.all(assets.map(async (asset) => {
-                                let data = await machinedata.find({ model_name: asset })
+                                let data = await Asset.find({ model_name: asset })
                                 return data[0]
                             }))
                             // replacing the original data with the mapped data
